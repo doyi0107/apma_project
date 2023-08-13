803bc2d16eabe62850bf1b6934983da47c14b1b6
jsmme <doyi9917@naver.com>
2023-08-13 18:41:46 +0900

    revise -ing

---
+++ js/index.js
@@ -0,0 +1,9 @@
+function mypage(){
+	let a = "1"
+	if(a = "1"){
+		alert("서비스는 로그인 후 사용하실 수 있습니다.");
+		location.href="./pages/login.html"
+	}else{
+		location.href="./login.html"
+	}
+}
--- js/login.js
+++ js/login.js
@@ -23,6 +23,17 @@
 // 		}
 // 	})
 // })
+function mypage(){
+	let a = "1"
+	if(a = "1"){
+		alert("서비스는 로그인 후 사용하실 수 있습니다.");
+		location.href="./login.html"
+	}else{
+		location.href="./login.html"
+	}
+}
+
+
 
 
 function signIn() {
@@ -54,30 +65,45 @@ function signIn() {
 	})
 }
 
-// var settings = {
-// 	"url": "http://127.0.0.1:5500/index.html",
-// 	"method": "POST",
-// 	"timeout": 0,
-// 	"headers": {
-// 	  "Content-Type": "application/json"
-// 	},
-// 	"data": JSON.stringify({
-// 	  "id_give": $('#userId').val(),
-// 	  "pw_give": $('#userPw').val()
-// 	})
-//   };
-  
-// 	  $.ajax(settings).done(function (response) {
-// 	  console.log(response);
-// 	  console.log(xhr.getREsponseHeader('Authorization'))// 헤더에 있는 토큰을 받아와서
-// 	  localStorage.setItem('accessToken', xhr.getREsponseHeader('Authorization')) // 로컬스토리지 = 디비 , 에다가 셋 에 응답 받은 토큰을 그대로 넣는다
-// 	  alert('로그인 완료')
-// 	  window.location = '../index.html' //로그인 성공시 새로고침 
-// 	  }).fail(function(response){
-// 		  console.log(response.responseJSON);
-// 		  if(response.responseJSON.statusCode === 404){
-// 			  alert('아이디와 비밀번호를 확인하여 주세요');
-// 		  } else{
-// 			  alert('서버에 문제가 발생하였습니다.');
-// 		  }
-// 	  });
+
+// 카카오 로그인
+
+Kakao.init('여기에 자바 스크립트 키를 입력하세요');
+console.log(Kakao.isInitialized());  // sdk초기화여부판단
+//카카오로그인
+function kakaoLogin() {
+	Kakao.Auth.login({
+		success: function (response) {
+			Kakao.API.request({
+				url: '/v2/user/me',
+				success: function (response) {
+					console.log(response)
+				},
+				fail: function (error) {
+					console.log(error)
+				},
+			})
+		},
+		fail: function (error) {
+			console.log(error)
+		},
+	})
+}
+
+
+//카카오로그아웃  
+
+function kakaoLogout() {
+    if (Kakao.Auth.getAccessToken()) {
+      Kakao.API.request({
+        url: '/v1/user/unlink',
+        success: function (response) {
+        	console.log(response)
+        },
+        fail: function (error) {
+          console.log(error)
+        },
+      })
+      Kakao.Auth.setAccessToken(undefined)
+    }
+  }  
